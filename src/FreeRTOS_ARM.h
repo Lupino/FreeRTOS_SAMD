bc4ccd7885c56332d228545a7d961ac61e7603e1
Lupino <lmjubuntu@gmail.com>
2017-09-19 15:53:22 +0800

    Initial Commit

---
+++ src/FreeRTOS_ARM.h
@@ -0,0 +1,12 @@
+#ifndef FreeRTOS_ARM_h
+#define FreeRTOS_ARM_h
+
+/** FreeRTOS_ARM version YYYYMMDD */
+#define FREE_RTOS_ARM_VERSION 20170919
+
+#include "kernel/FreeRTOS.h"
+#include "kernel/task.h"
+#include "kernel/queue.h"
+#include "kernel/semphr.h"
+#include "kernel/portmacro.h"
+#endif  // FreeRTOS_ARM_h
